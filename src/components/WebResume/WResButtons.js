6710a138a1e270fc880b1ec5507d5404570029e0
AutosaveDave <106124456+dccoppock@users.noreply.github.com>
2023-10-17 01:31:05 -0400

    Added view/download pdf resume functionality

--- src/components/WebResume/WResButtons.js
+++ src/components/WebResume/WResButtons.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { Box, Typography, Stack, Button, ButtonGroup } from "@mui/material";
+import ResumePdf from '../../utils/docs/AutosaveDaveResume.pdf';
 
-const WResButtons = ( { _name, _tagline } ) => {
+const WResButtons = () => {
+    const resumePdfUrl = ResumePdf;
+    const handleDownloadPdf = () => {
+        const downloadLink = document.createElement("a");
+        downloadLink.href = resumePdfUrl;
+        downloadLink.download = `David Coppock's Resume.pdf`;
+        downloadLink.target = '_blank';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    }
+    const handleViewPdf = () => {
+        const viewLink = document.createElement("a");
+        viewLink.href = resumePdfUrl;
+        viewLink.target = '_blank';
+        document.body.appendChild(viewLink);
+        viewLink.click();
+        document.body.removeChild(viewLink);
+    }
+
   return <>
     <Box position='fixed' bottom='1.5em' left={'2em'} right={'2em'} zIndex={2} textAlign='center'>
-        <ButtonGroup variant='resume' color='error' >
-            <Button>View pdf</Button>
-            <Button>Download pdf</Button>
+        <ButtonGroup variant='resume' >
+            <Button onClick={handleViewPdf}
+                
+            >View pdf</Button>
+            <Button onClick={handleDownloadPdf}
+            >Download pdf</Button>
         </ButtonGroup>    
     </Box>
   </>;
--- src/utils/icons/CarnegieMellonSeal.js
+++ src/utils/icons/CarnegieMellonSeal.js
@@ -3,6 +3,5 @@ import { SvgIcon } from "@mui/material";
 import { ReactComponent as CMUSealSvg } from './cmu-seal-r.svg';
 
 export default function CarnegieMellonSeal() {
-    // const walrusSx = { alignSelf: 'end', height: '64px', width: '64px', };
     return <SvgIcon component={ CMUSealSvg } inheritViewBox={ true } style={{ height:'100%', width:'100%' }}/>
 }
